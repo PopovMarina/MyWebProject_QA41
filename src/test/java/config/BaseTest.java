0b577b495f81bd2a55af1f9204858e47dd9d7b24
Ahmshere <asus555post@gmail.com>
2024-02-21 16:14:53 +0200

    comments
    have added comments

--- src/test/java/config/BaseTest.java
+++ src/test/java/config/BaseTest.java
@@ -13,18 +13,26 @@ import pages.BasePage;
 
 import java.time.Duration;
 
-public class BaseTest {
+public class BaseTest { // Эта строка объявляет начало определения класса BaseTest. Класс является шаблоном или чертежом для создания объектов.
     private static final ThreadLocal<WebDriver> driverThreadLocal
-            = new ThreadLocal<>();
+            = new ThreadLocal<>(); //Эта строка объявляет статическое приватное поле driverThreadLocal, которое является объектом класса ThreadLocal. Он используется для хранения объектов типа WebDriver в потоке исполнения.
+    // ThreadLocal - это класс в Java, который позволяет создавать локальные переменные, специфичные для каждого потока.
+    // Каждый поток имеет свою собственную копию переменной, хранящейся в ThreadLocal, и доступ может быть получен только из соответствующего потока.
+    // Это полезно, когда вам нужно создать объект, который будет уникальным для каждого потока, и при этом изолировать состояние между потоками.
 
     public static WebDriver getDriver(){
         return driverThreadLocal.get();
-    }
+    } //Этот метод возвращает экземпляр WebDriver из объекта driverThreadLocal. Он используется для получения веб-драйвера, установленного для текущего потока
 
 
+    // Эти строки аннотируют метод setUp как метод, который должен быть выполнен перед каждым тестовым методом (@BeforeMethod), и указывают, что он принимает параметр browser из файла конфигурации.
+    // Аннотация @Optional("firefox") означает, что значение по умолчанию для browser - это "firefox".
     @BeforeMethod
     @Parameters("browser")
     public void setUp(@Optional("firefox") String browser){
+
+        // Этот блок кода проверяет, является ли значение параметра browser равным "chrome".
+        // Если да, то он настраивает ChromeDriver и добавляет опции для запуска браузера на английском языке.
         if(browser.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
 
@@ -33,6 +41,8 @@ public class BaseTest {
             // options.addArguments("--headless");
             driverThreadLocal.set(new ChromeDriver(options));
         }
+        // Аналогично предыдущему блоку, но если browser равен "firefox",
+        // то настраивается FirefoxDriver и добавляются опции для запуска браузера на английском языке.
         else if(browser.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
             FirefoxOptions options = new FirefoxOptions();
@@ -42,6 +52,8 @@ public class BaseTest {
         }
         else{throw new IllegalArgumentException("Invalid browser "+browser); }
 
+        // Этот блок кода получает веб-драйвер с помощью метода getDriver(), максимизирует окно браузера,
+        // устанавливает время ожидания загрузки страницы и неявного ожидания, а затем устанавливает этот драйвер для BasePage.
         WebDriver driver = getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(20000));
