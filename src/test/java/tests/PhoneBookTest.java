76dbdd256abaf4fcf7f5871c3097c759ea01c3cc
Ahmshere <asus555post@gmail.com>
2024-02-23 13:04:22 +0200

    @new
    check

--- src/test/java/tests/PhoneBookTest.java
+++ src/test/java/tests/PhoneBookTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import config.BaseTest;
 import helpers.TopMenuItem;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.MainPage;
@@ -9,7 +10,8 @@ import pages.MainPage;
 public class PhoneBookTest extends BaseTest {
 
     @Test
-    public void phoneBookTest_001() throws InterruptedException {
+    @Parameters("browser")
+    public void phoneBookTest_001(String browser) throws InterruptedException {
         MainPage mainPage = new MainPage(getDriver());
         LoginPage loginPage = mainPage.openTopMenu(TopMenuItem.LOGIN.toString());
         loginPage.fillEmailField("myemail@mail.com").clickByRegistartionBUtton();
